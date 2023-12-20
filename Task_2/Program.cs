967fe4849be88696e651b1a8563a19c89314d313
Vlad <bodroon@gmail.com>
2023-12-20 17:13:51 +0300

    Добавил задачи с 1 по 4

---
+++ Task_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте массив на 10 целых чисел. 
+//Напишите программу, которая определяет количество чётных чисел в массиве.
+
+// int[] array = { 8, 3, 2, 6, 4, 1, 5, 4, 6, 4 }; 
+   
+int[] array = new int[10]; 
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1, 10);
+    Console.WriteLine(array[i]);
+}
+
+   int res = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 == 0) res++;
+            }
+Console.WriteLine("\nКоличество четных чисел в этом массиве равно: " + res);
---
+++ Task_3/Program.cs
@@ -0,0 +1,70 @@
+﻿// Задайте массив из вещественных чисел с ненулевой дробной частью. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+double[] GenerateArray(int size, int leftRange, int rightRange)
+{
+	double[] array = new double[size];
+	Random rand = new Random();
+
+	for (int i = 0; i < size; i++)
+	{
+		array[i] = Math.Round(rand.Next(leftRange, rightRange + 1) + rand.NextDouble(), 3); // генерим целое + дробную и округляем до 3-х знаков
+	}
+	return array;
+}
+
+void PrintArray(double[] arrayForPrint)
+{
+	System.Console.Write("[ ");
+	for (int i = 0; i < arrayForPrint.Length; i++)
+	{
+		System.Console.Write(arrayForPrint[i] + " ");
+	}
+	System.Console.Write("]");
+	System.Console.WriteLine();
+}
+
+int ReadInt(string msg)
+{
+	Console.Write(msg);
+	return Convert.ToInt32(Console.ReadLine());
+}
+
+int FindMinInArray(double[] array)
+{
+	int indexMin = 0;
+	for(int i = 0; i < array.Length; i++ )
+	{
+		if(array[i] < array[indexMin])
+			{
+				indexMin = i;
+			}
+	}
+	return indexMin;
+}
+
+int FindMaxInArray(double[] array)
+{
+	int indexMax = 0;
+	for(int i = 0; i < array.Length; i++ )
+	{
+		if(array[i] > array[indexMax])
+			{
+				indexMax = i;
+			}
+	}
+	return indexMax;
+}
+
+// --------------------------
+int N = ReadInt("Введите размер массива: ");
+
+double[] myArray = GenerateArray(N, 0, 9);
+PrintArray(myArray);
+
+int iMin = FindMinInArray(myArray);
+int iMax = FindMaxInArray(myArray);
+
+System.Console.WriteLine($"Разница между {myArray[iMin]} и {myArray[iMax]} равна: {myArray[iMax] - myArray[iMin]}");
+
+// System.Console.WriteLine(myArray.Max() - myArray.Min());
